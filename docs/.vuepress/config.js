a4d16d92b335d35a14a4055e1053acefcd981009
arvin <arvinzhujun@gmail.com>
2018-07-06 16:13:18 +0800

    init2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,11 +2,9 @@ module.exports = {
   title: 'Ar Blog',
   description: 'Just a blog',
   head: [
-    [['link',{rel: 'icon', href: '/favicon.ico'}]
+    ['link',{rel: 'icon', href: '/favicon.ico'}]
   ],
   base: '/',
-  host: '118.24.15.173',
-  theme: 'awesome',
   search: true,
   themeConfig: {
     nav: [
@@ -20,28 +18,24 @@ module.exports = {
       },
       { text: 'Github', link: 'https://www.github.com/' }
     ],
-    sidebar: {
-      '/css/': [
+    sidebar: [
 	{
-         title: 'front css',
+         title: ' css',
 	 collapsable: true,
          children: [
 	  '/css/demo',
           '/css/dem2'
 	 ]
-        }
-      ],
-      '/vue/':[
+        },
 	{
-         title: 'front vue',
+         title: ' vue',
 	 collapsable: true,
          children: [
 	  '/vue/demo',
           '/vue/dem2'
 	 ]
         }
-      ]
-    }
+    ]
   },
   sidebarDepth: 2,
   lastUpdated: 'Last Updated',
